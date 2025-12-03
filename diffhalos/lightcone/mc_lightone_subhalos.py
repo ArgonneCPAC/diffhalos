5a910d456f3d97e9acbd8dde42fff28d3d24fed0
Georgios Zacharegkas <gzacharegkas@gmail.com>
2025-12-03 13:10:34 -0600

    fixed halo weights in lightcone

---
+++ diffhalos/lightcone/mc_lightone_subhalos.py
@@ -0,0 +1,35 @@
+"""Functions to generate Monte Carlo realizations of subhalos in a lightcone"""
+
+from ..ccshmf.ccshmf_model import DEFAULT_CCSHMF_PARAMS
+
+
+__all__ = ("mc_weighted_subhalo_lightcone",)
+
+
+def mc_weighted_subhalo_lightcone(
+    logmp_obs,
+    lgmu_min,
+    lgmu_max,
+    num_subs,
+    ccshmf_params=DEFAULT_CCSHMF_PARAMS,
+):
+    """
+    Generate a subhalo lightcone
+
+    Parameters
+    ----------
+
+
+    Returns
+    -------
+    """
+
+    # get host halo masses in lightcone
+
+    # for each lgmhost, get <Nsub> (ccshmf[0])
+
+    # get total number of subhalos as sum(<Nsub>)
+
+    # draw
+
+    return
