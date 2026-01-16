f8d241abac7e6c69857819efbde5a8c4c0ae7e20
Andrew Hearin <ahearin@anl.gov>
2026-01-16 17:15:47 -0600

    python black changes

--- diffhalos/mah/diffmahnet/__init__.py
+++ diffhalos/mah/diffmahnet/__init__.py
@@ -1,4 +1,8 @@
 from .diffmahnet import (  # noqa: F401
-    DiffMahFlow, Scaler, gen_time_grids, log_mah_kern,
-    pretrained_model_names, load_pretrained_model,
+    DiffMahFlow,
+    Scaler,
+    gen_time_grids,
+    log_mah_kern,
+    pretrained_model_names,
+    load_pretrained_model,
 )
