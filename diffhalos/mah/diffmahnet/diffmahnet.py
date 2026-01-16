84a3af26ed558a5f4940cd9ff51937ed6189ce71
Georgios Zacharegkas <gzacharegkas@gmail.com>
2026-01-16 14:34:11 -0600

    migrating diffmahnet into diffhalos

--- diffhalos/mah/diffmahnet/diffmahnet.py
+++ diffhalos/mah/diffmahnet/diffmahnet.py
@@ -6,10 +6,8 @@ import jax
 import jax.numpy as jnp
 import numpy as np
 import flowjax
-# import tqdm.auto as tqdm
 
 import flowjax.distributions
-import flowjax.bijections
 import flowjax.flows
 import flowjax.train
 import equinox as eqx
@@ -19,18 +17,18 @@ from diffopt import kdescent
 
 import diffmah
 from diffmah import DEFAULT_MAH_PARAMS
-from diffmah.diffmah_kernels import (
-    get_unbounded_mah_params, get_bounded_mah_params)
+from diffmah.diffmah_kernels import get_unbounded_mah_params, get_bounded_mah_params
+
 DEFAULT_MAH_UPARAMS = get_unbounded_mah_params(DEFAULT_MAH_PARAMS)
 
 
-log_mah_kern = jax.jit(jax.vmap(
-    diffmah.diffmah_kernels._log_mah_kern, in_axes=(0, 0, None)))
+log_mah_kern = jax.jit(
+    jax.vmap(diffmah.diffmah_kernels._log_mah_kern, in_axes=(0, 0, None))
+)
 
 pretrained_path = pathlib.Path(__file__).parent / "pretrained_models"
 pretrained_model_names = glob.glob(str(pretrained_path / "*.eqx"))
-pretrained_model_names = [str(pathlib.Path(x).name)
-                          for x in pretrained_model_names]
+pretrained_model_names = [str(pathlib.Path(x).name) for x in pretrained_model_names]
 
 
 def load_pretrained_model(name):
@@ -50,7 +48,8 @@ def load_pretrained_model(name):
     """
     if name not in pretrained_model_names:
         raise ValueError(
-            f"{name=} not found. Available models: {pretrained_model_names}")
+            f"{name=} not found. Available models: {pretrained_model_names}"
+        )
 
     filename = pretrained_path / name
     return DiffMahFlow.load(filename)
@@ -60,11 +59,11 @@ def gen_time_grids(key, t_obs, t_min=0.1, n_tgrid=20):
     key1, key2 = jax.random.split(key, 2)
     fixed_tgrid = jnp.linspace(t_min, t_obs, n_tgrid).T
     t_min_dither = jax.random.uniform(
-        key1, t_obs.shape, minval=fixed_tgrid[:, 0],
-        maxval=fixed_tgrid[:, 1])
+        key1, t_obs.shape, minval=fixed_tgrid[:, 0], maxval=fixed_tgrid[:, 1]
+    )
     t_max_dither = jax.random.uniform(
-        key2, t_obs.shape, minval=fixed_tgrid[:, -2],
-        maxval=fixed_tgrid[:, -1])
+        key2, t_obs.shape, minval=fixed_tgrid[:, -2], maxval=fixed_tgrid[:, -1]
+    )
     return np.linspace(t_min_dither, t_max_dither, n_tgrid).T
 
 
@@ -89,7 +88,7 @@ def make_flatten_and_unflatten_funcs(param_tree):
         x = []
         index = 0
         for shape, size in zip(shapes, sizes):
-            xi = flatx[index:(index := index + size)]
+            xi = flatx[index : (index := index + size)]
             x.append(jnp.reshape(xi, shape))
         return jax.tree.unflatten(treedef, x)
 
@@ -115,31 +114,34 @@ class DiffMahFlow:
         Random key for reproducibility, by default None
     """
 
-    def __init__(self, scaler, nn_depth=2, nn_width=50, flow_layers=8,
-                 randkey=None):
+    def __init__(self, scaler, nn_depth=2, nn_width=50, flow_layers=8, randkey=None):
         x_dim = scaler.x_scaler.n_features_in_
         cond_dim = scaler.u_scaler.n_features_in_
         self.scaler = scaler
         self.randkey = jax.random.key(0) if randkey is None else randkey
         self.flow = flowjax.flows.masked_autoregressive_flow(
-            key=self.randkey, invert=False,
+            key=self.randkey,
+            invert=False,
             base_dist=flowjax.distributions.Normal(jnp.zeros(x_dim)),
-            cond_dim=cond_dim, nn_depth=nn_depth,
-            nn_width=nn_width, flow_layers=flow_layers
+            cond_dim=cond_dim,
+            nn_depth=nn_depth,
+            nn_width=nn_width,
+            flow_layers=flow_layers,
         )
         self.nn_depth = nn_depth
         self.nn_width = nn_width
         self.flow_layers = flow_layers
         param_tree, self.static = self._partition()
-        self.flatten, self.unflatten = make_flatten_and_unflatten_funcs(
-            param_tree)
+        self.flatten, self.unflatten = make_flatten_and_unflatten_funcs(param_tree)
 
-    def get_tgrid_and_log_mah(self, m_obs, t_obs, randkey,
-                              t_min=0.1, n_tgrid=20, t0=13.8, extra_shape=()):
+    def get_tgrid_and_log_mah(
+        self, m_obs, t_obs, randkey, t_min=0.1, n_tgrid=20, t0=13.8, extra_shape=()
+    ):
         key1, key2 = jax.random.split(randkey)
         u = jnp.array([m_obs, t_obs]).T
         mah_params = self.sample(
-            u, randkey=key1, extra_shape=extra_shape, asparams=True)
+            u, randkey=key1, extra_shape=extra_shape, asparams=True
+        )
 
         tgrid = gen_time_grids(key2, t_obs, t_min=t_min, n_tgrid=n_tgrid)
         log_mah = log_mah_kern(mah_params, tgrid, np.log10(t0))
@@ -149,13 +151,17 @@ class DiffMahFlow:
         @jax.jit
         def mc_diffmahnet(flow_params, lgm_obs, t_obs, ran_key):
             return self.sample(
-                jnp.array([lgm_obs, t_obs]).T, randkey=ran_key,
-                asparams=True, flow_params=flow_params)
+                jnp.array([lgm_obs, t_obs]).T,
+                randkey=ran_key,
+                asparams=True,
+                flow_params=flow_params,
+            )
 
         return mc_diffmahnet
 
-    def sample(self, condition, randkey=None, extra_shape=(), asparams=False,
-               flow_params=None):
+    def sample(
+        self, condition, randkey=None, extra_shape=(), asparams=False, flow_params=None
+    ):
         """
         Sample diffmah u_params, conditioned on (m_obs, t_obs)
 
@@ -185,13 +191,10 @@ class DiffMahFlow:
         condition_scaled = scaler_transform(condition, self.scaler.u_scaler)
         if randkey is None:
             randkey, self.randkey = jax.random.split(self.randkey, 2)
-        x_scaled = flow.sample(
-            randkey, extra_shape, condition=condition_scaled)
-        uparam_array = scaler_transform(
-            x_scaled, self.scaler.x_scaler, inverse=True)
+        x_scaled = flow.sample(randkey, extra_shape, condition=condition_scaled)
+        uparam_array = scaler_transform(x_scaled, self.scaler.x_scaler, inverse=True)
         if asparams:
-            return get_bounded_mah_params(
-                DEFAULT_MAH_UPARAMS._make(uparam_array.T))
+            return get_bounded_mah_params(DEFAULT_MAH_UPARAMS._make(uparam_array.T))
         else:
             return uparam_array
 
@@ -217,7 +220,7 @@ class DiffMahFlow:
             nn_depth=self.nn_depth,
             nn_width=self.nn_width,
             flow_layers=self.flow_layers,
-            **self.scaler.to_dict()
+            **self.scaler.to_dict(),
         )
 
         filename = str(filename).removesuffix(".eqx") + ".eqx"
@@ -252,30 +255,53 @@ class DiffMahFlow:
             hyperparams["flow_layers"] = int(hyperparams["flow_layers"])
             scaler = Scaler.from_dict(hyperparams)
             self = cls(
-                scaler=scaler, nn_depth=hyperparams["nn_depth"],
+                scaler=scaler,
+                nn_depth=hyperparams["nn_depth"],
                 nn_width=hyperparams["nn_width"],
-                flow_layers=hyperparams["flow_layers"], randkey=randkey)
+                flow_layers=hyperparams["flow_layers"],
+                randkey=randkey,
+            )
             self.flow = eqx.tree_deserialise_leaves(f, self.flow)
 
         self._reset_static()
         return self
 
-    def init_fit(self, xtrain, utrain, randkey=None,
-                 learning_rate=1e-2, max_patience=10, max_epochs=50):
+    def init_fit(
+        self,
+        xtrain,
+        utrain,
+        randkey=None,
+        learning_rate=1e-2,
+        max_patience=10,
+        max_epochs=50,
+    ):
         """Train the flow directly on P(mah_params|m_obs,t_obs)"""
         x_scaled = scaler_transform(xtrain, self.scaler.x_scaler)
         u_scaled = scaler_transform(utrain, self.scaler.u_scaler)
         if randkey is None:
             randkey, self.randkey = jax.random.split(self.randkey, 2)
         self.flow, losses = flowjax.train.fit_to_data(
-            randkey, self.flow, x_scaled, condition=u_scaled,
-            learning_rate=learning_rate, max_patience=max_patience,
-            max_epochs=max_epochs)
+            randkey,
+            self.flow,
+            x_scaled,
+            condition=u_scaled,
+            learning_rate=learning_rate,
+            max_patience=max_patience,
+            max_epochs=max_epochs,
+        )
         self._reset_static()
         return losses
 
-    def adam_fit(self, lossfunc, randkey=None, nsteps=100,
-                 progress=True, learning_rate=1e-4, thin=1, **kwargs):
+    def adam_fit(
+        self,
+        lossfunc,
+        randkey=None,
+        nsteps=100,
+        progress=True,
+        learning_rate=1e-4,
+        thin=1,
+        **kwargs,
+    ):
         """Fit the flow using the Adam stochastic gradient descent
 
         Parameters
@@ -307,10 +333,17 @@ class DiffMahFlow:
         def lossfunc_from_params(flat_params, randkey=randkey):
             self.set_params(flat_params)
             return lossfunc(self, randkey=randkey)
+
         adam_params, adam_losses = kdescent.adam(
-            lossfunc_from_params, self.get_params(), nsteps=nsteps,
-            progress=progress, randkey=randkey, learning_rate=learning_rate,
-            thin=thin, **kwargs)
+            lossfunc_from_params,
+            self.get_params(),
+            nsteps=nsteps,
+            progress=progress,
+            randkey=randkey,
+            learning_rate=learning_rate,
+            thin=thin,
+            **kwargs,
+        )
         self.set_params(adam_params[-1])
         self.static = self._partition()[1]
 
@@ -318,17 +351,18 @@ class DiffMahFlow:
 
     def _partition(self):
         return eqx.partition(
-            self.flow, eqx.is_inexact_array,
-            is_leaf=lambda leaf: isinstance(leaf, paramax.NonTrainable))
+            self.flow,
+            eqx.is_inexact_array,
+            is_leaf=lambda leaf: isinstance(leaf, paramax.NonTrainable),
+        )
 
     def _flow_from_flat_params(self, flat_params):
         param_tree = self.unflatten(flat_params)
-        return (eqx.combine(param_tree, self.static))
+        return eqx.combine(param_tree, self.static)
 
     def _reset_static(self):
         param_tree, self.static = self._partition()
-        self.flatten, self.unflatten = make_flatten_and_unflatten_funcs(
-            param_tree)
+        self.flatten, self.unflatten = make_flatten_and_unflatten_funcs(param_tree)
 
 
 class _StandardScaler:
@@ -372,5 +406,5 @@ class Scaler:
             x_mean=self.x_scaler.mean_.tolist(),
             x_scale=self.x_scaler.scale_.tolist(),
             u_mean=self.u_scaler.mean_.tolist(),
-            u_scale=self.u_scaler.scale_.tolist()
+            u_scale=self.u_scaler.scale_.tolist(),
         )
