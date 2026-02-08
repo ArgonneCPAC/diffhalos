870279ba0c64cd3aa5463b0adf0a7a9adbb8ec3a
Andrew Hearin <ahearin@anl.gov>
2026-02-08 12:55:13 -0600

    Add generator for host halos only. Rename diffhalos.lightcone to lightcone_generators

--- diffhalos/__init__.py
+++ diffhalos/__init__.py
@@ -4,5 +4,5 @@
 
 from ._version import __version__
 from .defaults import LOGT0_GYR, T0_GYR
-from .lightcone import weighted_lc
+from .lightcone_generators import weighted_lc, weighted_lc_halos
 from .utils import redshift_mass_grid
